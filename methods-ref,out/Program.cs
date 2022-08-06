cd8d4977f9932bea12c912b72654fb6b8b4fe84a
Lemanyaqubova <lamanny@code.edu.az>
2022-08-06 17:16:09 +0400

    task

---
+++ methods-ref,out/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace methods_ref_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 16;
+            Console.WriteLine(NumLent(num));
+        }
+        static int NumLent (int num)
+        {
+            num = 16;
+            int count = 0;
+            while (num > 0) 
+            
+            {
+                num/= 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}
